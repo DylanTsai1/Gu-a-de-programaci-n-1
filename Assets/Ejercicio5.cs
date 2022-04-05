2a37d46190c579fd766c3026265f007253735804
DylanTsai1 <101568962+DylanTsai1@users.noreply.github.com>
2022-04-05 07:58:20 -0300

    Ejercicios 2, 3, 4, 5, 8 Terminados

---
+++ Assets/Ejercicio5.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio5 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("La división entre " + num1 + "y" + num2 + "es de" + (num1/num2)); 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
