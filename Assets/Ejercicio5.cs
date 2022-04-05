26f92f01a8cbcdff1a55d56abd42ce0318e476e1
DylanTsai1 <101568962+DylanTsai1@users.noreply.github.com>
2022-04-05 08:39:01 -0300

    Ejercicio 5 Terminado

--- Assets/Ejercicio5.cs
+++ Assets/Ejercicio5.cs
@@ -10,8 +10,16 @@ public class Ejercicio5 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("La división entre " + num1 + "y" + num2 + "es de" + (num1/num2)); 
 
+        if (num2 == 0)
+        {
+            Debug.Log("No se puede dividir por 0");
+
+        }
+        else
+        {
+            Debug.Log("La división entre " + num1 + "y" + num2 + "es de" + (num1 / num2));
+        }
     }
 
     // Update is called once per frame
