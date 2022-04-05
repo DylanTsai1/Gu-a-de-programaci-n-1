6359a5000117f2eb3b36dd0f148f5953b2f07327
DylanTsai1 <101568962+DylanTsai1@users.noreply.github.com>
2022-04-05 09:09:20 -0300

    Ejercicio9 Terminado

---
+++ Assets/Ejercicio9.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio9 : MonoBehaviour
+{
+    public string nom1;
+    public string nom2;
+    public string nom3;
+
+    public float per1;
+    public float per2;
+    public float per3;
+
+    float capitalTotal;
+    float porc1;
+    float porc2;
+    float porc3;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    { capitalTotal = per1 + per2 + per3;
+
+        porc1 = per1/capitalTotal * 100;
+        porc2 = per2 / capitalTotal * 100;
+        porc3 = per3 / capitalTotal * 100;
+
+        Debug.Log("Nombre: " + nom1 + " capital aportado: $ " + per1 + ", Porcentaje del capital: %" + porc1 + ", Monto  total aportado: $" + capitalTotal );
+        Debug.Log("Nombre: " + nom2 + " capital aportado: $ " + per2 + ", Porcentaje del capital: %" + porc2 + ", Monto  total aportado: $" + capitalTotal);
+        Debug.Log("Nombre: " + nom3 + " capital aportado: $ " + per3 + ", Porcentaje del capital: %" + porc3 + ", Monto  total aportado: $" + capitalTotal);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
