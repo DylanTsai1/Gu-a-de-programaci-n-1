696ce2bbe88569aae90049fd4f9deb40458f7411
DylanTsai1 <101568962+DylanTsai1@users.noreply.github.com>
2022-04-05 08:49:19 -0300

    Ejercicio6 Terminado

---
+++ Assets/Ejercicio6.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio6 : MonoBehaviour
+{
+    public int num1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1 % 2 == 1)
+        {
+            Debug.Log("El número es impar");
+        }
+        else
+            Debug.Log("El número es par");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
